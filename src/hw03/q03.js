87e193ee67125aba13f5c06ef084023b961a45ca
Yovanaedith <yovanaedith2000@gmail.com>
2024-10-16 22:58:31 -0400

    Solution LAB-5

---
+++ src/hw03/q03.js
@@ -0,0 +1,24 @@
+const link = document.querySelector("a");
+link.href = "https://keybr.com";
+
+const buyList = [
+    "books",
+    "bread",
+    "eraser",
+    "earphones",
+    "collection-cards",
+    "hdmi",
+    "vga",
+    "motherboard",
+    "university-books",
+];
+  
+const ul = document.createElement("ul");
+buyList.forEach(item => {
+    const li = document.createElement("li");
+    li.textContent = item;
+    ul.appendChild(li);
+});
+  
+root.appendChild(ul);
+  
